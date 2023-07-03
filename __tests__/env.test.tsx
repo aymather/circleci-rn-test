76bf1311b7d6ee1e5e04cf2e9ba851daa270858b
aymather <aymather@gmail.com>
2023-07-03 10:41:14 -0700

    still testing

--- __tests__/env.test.tsx
+++ __tests__/env.test.tsx
@@ -2,6 +2,9 @@ import Config from 'react-native-config'
 
 describe('Environment Variables', () => {
     it('API_URL is set correctly', () => {
-        expect(Config.API_URL).toBe(undefined)  // replace 'expected value' with the expected value of your variable
+        expect(Config.API_URL).toBe(undefined)
+    })
+    it('TEST is set correctly', () => {
+        expect(Config.TEST).toBe(undefined)
     })
 })
